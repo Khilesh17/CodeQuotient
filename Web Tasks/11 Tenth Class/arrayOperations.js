aef8a3f4e448f6eb6c0e68b3c10c9e230b328721
Khilesh <Khilesh093@gmail.com>
2024-04-06 11:12:54 +0530

    spinner

--- Web Tasks/11 Tenth Class/arrayOperations.js
+++ Web Tasks/11 Tenth Class/arrayOperations.js
@@ -1,9 +1,42 @@
 // map
 // filter
-// foreach 
+// foreach
 // findIndex
-// find 
-// reduce 
-// splice 
-// slice 
-// sort 
+// find
+// reduce
+// splice
+// slice
+// sort
+
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map(num => num * 2);
+console.log("Example of Map : ", doubledNumbers);
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log("Example of Filter : ", evenNumbers);
+
+console.log("Example of forEach : ")
+numbers.forEach(num => console.log(num)); // Output: 1 2 3 4 5
+
+const index = numbers.findIndex(num => num > 2);
+console.log("Example of FindIndex : ", index);
+
+
+const value = numbers.find(num => num > 2);
+console.log("Example of Find : ", value);
+
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log("Example of Reduce : ", sum);
+
+
+const slicedNumbers = numbers.slice(2, 4); 
+console.log("Example of slice : ", slicedNumbers);
+
+
+numbers.sort((a, b) => a - b);
+console.log("Example of sort : ", numbers);
+
+
+numbers.splice(2, 1);
+console.log("Example of splice : ", numbers);
