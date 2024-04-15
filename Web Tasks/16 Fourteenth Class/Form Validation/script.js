2d5b0db2817ba9f3258aa414dd57a0bf5d47eb60
Khilesh <Khilesh093@gmail.com>
2024-04-15 22:09:23 +0530

    Done

--- Web Tasks/16 Fourteenth Class/Form Validation/script.js
+++ Web Tasks/16 Fourteenth Class/Form Validation/script.js
@@ -5,6 +5,7 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const phoneNumber = document.querySelector("#phoneNumber");
 
+const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -19,16 +20,24 @@ form.addEventListener("submit", (event) => {
     if (nameValue.trim() === '' || emailValue.trim() === '' || passwordValue.trim() === '' || phoneNumberValue.trim() === '') {
         validationResult.textContent = "All Fields are Required";
         validationResult.style.color = "red";
+        return;
+    }
+
+
+    if (emailRegex.test(emailValue)) {
+        console.log("Valid email address");
     }
     else {
-        validationResult.textContent = "Values are shown in Console"
-        validationResult.style.color = "green";
-        console.log(`Values are : 
-            Name : ${nameValue}
-            Email : ${emailValue}
-            Password : ${passwordValue}
-            Phone No. : ${phoneNumberValue}`
-        );
+        console.log("Invalid email address");
     }
+        
+    validationResult.textContent = "Values are shown in Console"
+    validationResult.style.color = "green";
+    console.log(`Values are : 
+        Name : ${nameValue}
+        Email : ${emailValue}
+        Password : ${passwordValue}
+        Phone No. : ${phoneNumberValue}`
+    );
     
 })
